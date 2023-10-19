5bac6888a2e861aae98172865397540a2029f85c
perdomoper <gustavo.perdomo@estudante.ifms.edu.br>
2023-10-19 16:11:31 -0400

    ds

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -1,15 +1,29 @@
+import { useState } from 'react'
 import './Contato.css'
 
 export function Contato(){
+    const [texto,setTexto]=useState("")
+    function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
+        setTexto(e.target.value)}
+
   return(
+    <>
             <div className="formulario">
                 <h1 className="titulo">Contato</h1>
-            <input className="nome" type="text" placeholder="Nome:"/>
+                <div className='pes'>
+            <input className='pesquisa' 
+            type="text" 
+            placeholder='O que você está procurando?'
+            onChange={TrataTexto} />
+            {texto && <p className='pesquisa1'>Resultados Para: {texto} </p>}
+                
+        </div>
             <input className="email" type="text" placeholder="Email:"/>
             <input className="telefone" type="text" placeholder="Telefone:" />
             <input className="mensagem" type="text" placeholder="Mensagem:"/>
             <button className="enviar">Enviar</button>
             </div>
+            </>
   )
 }
 
