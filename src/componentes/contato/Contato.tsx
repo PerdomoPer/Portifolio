218ed40cf94c0d7edd07ee3eae85092ee618ad41
perdomoper <gustavo.perdomo@estudante.ifms.edu.br>
2023-10-19 16:02:09 -0400

    f

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -2,16 +2,14 @@ import './Contato.css'
 
 export function Contato(){
   return(
-    <div className="Adess">
-            <div className="Adess2">
-                <h1 className="titulo">ESCREVA SUA MENSAGEM</h1>
-            <input className="nome" type="text" placeholder="Nome"/>
-            <input className="e-mail" type="text" placeholder="E-mail"/>
-            <input className="telefone" type="text" placeholder="Telefone" />
-            <input className="campo-mensagem" type="text" placeholder="Escreva sua mensagem aqui"/>
-            <button className="button-envio">Enviar</button>
+            <div className="formulario">
+                <h1 className="titulo">Contato</h1>
+            <input className="nome" type="text" placeholder="Nome:"/>
+            <input className="email" type="text" placeholder="Email:"/>
+            <input className="telefone" type="text" placeholder="Telefone:" />
+            <input className="mensagem" type="text" placeholder="Mensagem:"/>
+            <button className="enviar">Enviar</button>
             </div>
-        </div>
   )
 }
 
