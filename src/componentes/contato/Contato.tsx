623cb75360f2ed47933ef36119b2b64d32aa18d9
PerdomoPer <141922899+PerdomoPer@users.noreply.github.com>
2023-10-20 10:11:10 -0400

    Update Contato.tsx

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -1,19 +1,45 @@
+import { useState } from 'react'
 import './Contato.css'
 
 export function Contato(){
-    
+  const [nome,setNome]=useState("")
+  const [email,setEmail]=useState("")
+  const [telefone,setTelefone]=useState("")
+  const [mensagem,setMensagem]=useState("")
+  
+  function TrataNome(e:React.ChangeEvent<HTMLInputElement>){
+    setNome(e.target.value)
+}
+function TrataEmail(e:React.ChangeEvent<HTMLInputElement>){
+  setEmail(e.target.value)
+}
+function TrataTelefone(e:React.ChangeEvent<HTMLInputElement>){
+  setTelefone(e.target.value)
+}
+function TrataMensagem(e:React.ChangeEvent<HTMLInputElement>){
+  setMensagem(e.target.value)
+}
   return(
     
              <div className="formulario">
                 <h1 className="titulo">Contate-me</h1> 
                 <label htmlFor="text">Nome:</label>
-                <input className="box" type="text" placeholder="Digite seu Nome:"/>
+                <input className="box" type="text" placeholder="Digite seu Nome:"
+            onChange={TrataNome} />
+            {nome && <p className='box2'> {nome} </p>}
+          
                 <label htmlFor="text">Email:</label>
-                <input className="box" type="text" placeholder="Digite um endereço de email valido:"/>
+                <input className="box" type="text" placeholder="Digite um endereço de email valido:" onChange={TrataEmail} />
+            {email && <p className='box2'> {email} </p>}
+
                 <  label htmlFor="text">Telefone:</label>
-                <input className="box" type="text" placeholder="Digite seu numero de telefone no formato (xx) xxxxx-xxxx:" />
+                <input className="box" type="text" placeholder="Digite seu numero de telefone no formato (xx) xxxxx-xxxx:" onChange={TrataTelefone} />
+            {telefone && <p className='box2'> {telefone} </p>}
+
                 <label htmlFor="text">Mensagem:</label>
-                <input className="box-msg" type="text" placeholder="Escreve sua mensagem:"/>
+                <input className="box-msg" type="text" placeholder="Escreve sua mensagem:" onChange={TrataMensagem} />
+            {mensagem && <p className='box2'> {mensagem} </p>}
+
                 <button className="enviar">Enviar</button>
           </div>
  )
